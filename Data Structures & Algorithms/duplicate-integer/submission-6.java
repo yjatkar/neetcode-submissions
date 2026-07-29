db40e16453012e5412d01018835e73dbcfde8cc0
yjatkar <126138675+yjatkar@users.noreply.github.com>
2026-07-29 22:06:54 +0530

    Add: duplicate-integer - submission-6

---
+++ Data Structures & Algorithms/duplicate-integer/submission-6.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        int n=nums.length;
+        Arrays.sort(nums);
+        for(int i=0;i<n-1;i++)
+        {
+            if(nums[i]==nums[i+1])
+            {
+                return true;
+            }
+        }
+        return false;
+        
+    }
+}
